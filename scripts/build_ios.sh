19a1323ec078ba25640853d0d452d01667e6c1b6
Tyler Nijmeh <tylernij@gmail.com>
2025-12-17 09:20:46 -0800

    build_ios: Support cocoapods
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- scripts/build_ios.sh
+++ scripts/build_ios.sh
@@ -4,12 +4,26 @@
 build_ios() {
     local name="${1:-demo}"
 
-    xcodebuild archive \
-        -project "$name.xcodeproj" \
-        -scheme "$name" \
-        -configuration Release \
-        -archivePath "build/$name.xcarchive" \
-        CODE_SIGN_IDENTITY="" \
-        CODE_SIGNING_REQUIRED=NO \
-        CODE_SIGNING_ALLOWED=NO
+    # If there is a Podfile, use the workspace.
+    if [ -f "Podfile" ]; then
+        pod install
+
+        xcodebuild archive \
+            -workspace "$name.xcworkspace" \
+            -scheme "$name" \
+            -configuration Release \
+            -archivePath "build/$name.xcarchive" \
+            CODE_SIGN_IDENTITY="" \
+            CODE_SIGNING_REQUIRED=NO \
+            CODE_SIGNING_ALLOWED=NO
+    else
+        xcodebuild archive \
+            -project "$name.xcodeproj" \
+            -scheme "$name" \
+            -configuration Release \
+            -archivePath "build/$name.xcarchive" \
+            CODE_SIGN_IDENTITY="" \
+            CODE_SIGNING_REQUIRED=NO \
+            CODE_SIGNING_ALLOWED=NO
+    fi
 }
